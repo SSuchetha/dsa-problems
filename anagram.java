dbab543f66ed93483e0cef0e1455233174a68dce
Suchetha <83269185+SSuchetha@users.noreply.github.com>
2021-12-05 14:45:58 +0530

    Create anagram.java
    Given two strings s and t, return true if t is an anagram of s, and false otherwise.

---
+++ anagram.java
@@ -0,0 +1,24 @@
+class Solution 
+{
+    public boolean isAnagram(String s, String t) 
+    {
+        if(s.length()!=t.length())
+            return false;
+        char []s1=s.toCharArray();
+        char []t1=t.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+        for(int i=0; i<s1.length; i++)
+            if(s1[i]!=t1[i])
+                return false;
+        return true;
+    }
+    public static void main(String [] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        String t=sc.next();
+        boolean ans=isAnagram(s,t);
+        System.out.println(ans);
+    }
+}
