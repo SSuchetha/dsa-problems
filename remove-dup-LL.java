c5a2143988d65856f4b82750324232c0e846bab0
Suchetha <83269185+SSuchetha@users.noreply.github.com>
2021-12-07 19:52:26 +0530

    Create remove-dup-LL.java
    Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.

---
+++ remove-dup-LL.java
@@ -0,0 +1,15 @@
+class Solution 
+{
+    public ListNode deleteDuplicates(ListNode head) 
+    {
+        ListNode cur=head;
+        while(cur!=null)
+        {
+            if(cur.next!=null && cur.next.val==cur.val)
+                cur.next=cur.next.next;
+            else
+                cur=cur.next;
+        }
+        return head;
+    }
+}
